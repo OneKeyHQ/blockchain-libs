ea2f0dd3f059e056ac594643210ea5e68bc649e5
huazhouwang <huazhou19@gmail.com>
2021-12-19 16:07:54 +0800

    feat: create BaseRestfulClient for restful api

--- src/provider/abc.ts
+++ src/provider/abc.ts
@@ -1,6 +1,7 @@
 import BigNumber from 'bignumber.js';
 
 import { NotImplementedError } from '../basic/exceptions';
+import { check } from '../basic/precondtion';
 import { ChainInfo, CoinInfo } from '../types/chain';
 import {
   AddressInfo,
@@ -47,6 +48,93 @@ abstract class BaseClient {
   }
 
   getUTXOs(addresses: Array<string>): Promise<{ [address: string]: UTXO[] }> {
+    return Promise.reject(NotImplementedError);
+  }
+}
+
+abstract class BaseRestfulClient extends BaseClient {
+  async batchCall2SingleCall<T, R>(
+    inputs: T[],
+    handler: (input: T) => Promise<R | undefined>,
+  ): Promise<Array<R | undefined>> {
+    const outputs = await Promise.allSettled(inputs.map(handler));
+    check(
+      inputs.length === outputs.length,
+      `Batch call ${inputs.length} requests, but received ${outputs.length} results`,
+    );
+
+    return outputs.map((result, index) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      } else {
+        console.debug(
+          `Error in Calling ${handler.name}. `,
+          ' input: ',
+          inputs[index],
+          ', reason',
+          result.reason,
+        );
+      }
+    });
+  }
+
+  async getAddresses(
+    addresses: Array<string>,
+  ): Promise<Array<AddressInfo | undefined>> {
+    return this.batchCall2SingleCall(addresses, (i) => this.getAddress(i));
+  }
+
+  abstract getAddress(address: string): Promise<AddressInfo>;
+
+  getBalances(
+    requests: Array<{ address: string; coin: Partial<CoinInfo> }>,
+  ): Promise<Array<BigNumber | undefined>> {
+    return this.batchCall2SingleCall(requests, (i) =>
+      this.getBalance(i.address, i.coin),
+    );
+  }
+
+  abstract getBalance(
+    address: string,
+    coin: Partial<CoinInfo>,
+  ): Promise<BigNumber>;
+
+  getTransactionStatuses(
+    txids: Array<string>,
+  ): Promise<Array<TransactionStatus | undefined>> {
+    return this.batchCall2SingleCall(txids, (i) =>
+      this.getTransactionStatus(i),
+    );
+  }
+
+  abstract getTransactionStatus(txid: string): Promise<TransactionStatus>;
+
+  getTokenInfos(
+    tokenAddresses: Array<string>,
+  ): Promise<Array<PartialTokenInfo | undefined>> {
+    return this.batchCall2SingleCall(tokenAddresses, (i) =>
+      this.getTokenInfo(i),
+    );
+  }
+
+  getTokenInfo(tokenAddress: string): Promise<PartialTokenInfo | undefined> {
+    return Promise.reject(NotImplementedError);
+  }
+
+  async getUTXOs(
+    addresses: Array<string>,
+  ): Promise<{ [address: string]: UTXO[] }> {
+    const result = await this.batchCall2SingleCall(addresses, (i) =>
+      this.getUTXO(i),
+    );
+    return result.reduce<{ [address: string]: UTXO[] }>((acc, utxos, index) => {
+      const curAddress = addresses[index];
+      acc[curAddress] = utxos || [];
+      return acc;
+    }, {});
+  }
+
+  getUTXO(address: string): Promise<UTXO[]> {
     return Promise.reject(NotImplementedError);
   }
 }
@@ -102,4 +190,4 @@ abstract class BaseProvider {
   }
 }
 
-export { BaseClient, BaseProvider, ClientFilter };
+export { BaseClient, BaseRestfulClient, BaseProvider, ClientFilter };
