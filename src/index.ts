6658f077a2600afd73ac868f02e15f2fa20a621b
LiuZJ <hust_lzj@icloud.com>
2022-03-30 20:28:42 +0800

    feat: use ethers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { StorageLike, ExternalConfig } from './types/external-config';
 export type { Wallet } from './types/wallet';
 export { BlockchainEngine } from './engine';
+export { ethers } from 'ethers';
