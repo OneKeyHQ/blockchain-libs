6e4ee075b661a081ec4549b820450aa04acd89b2
ll__ <loatheb.zhao@gmail.com>
2022-09-02 09:30:13 +0800

    fix: remove ethers project at RPCProvider (#75)
    * Revert "feat: add ethers.js provider on geth"
    
    This reverts commit 9cac92c45737a0c23991e83286da1fbb1fb90798.
    
    * chore: update v0.0.42 version

--- src/provider/chains/eth/geth.ts
+++ src/provider/chains/eth/geth.ts
@@ -1,6 +1,5 @@
 import { defaultAbiCoder } from '@ethersproject/abi';
 import BigNumber from 'bignumber.js';
-import { providers } from 'ethers';
 import memoizee from 'memoizee';
 
 import { fromBigIntHex } from '../../../basic/bignumber-plus';
@@ -51,12 +50,10 @@ class Geth extends BaseClient {
   static readonly __LAST_BLOCK__ = 'latest';
   private _mmFee!: MmFee;
   readonly rpc: JsonRPCRequest;
-  readonly ethersProvider: providers.JsonRpcProvider;
 
   constructor(url: string) {
     super();
     this.rpc = new JsonRPCRequest(url);
-    this.ethersProvider = new providers.JsonRpcProvider(url);
   }
 
   get mmFee(): MmFee {
